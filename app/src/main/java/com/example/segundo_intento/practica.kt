820d18facf2c2430d26274ef3d04701e16cab0c2
Edivazquez <vazquez_edgar13@hotmail.com>
2024-07-10 11:22:18 -0600

    prmer cambio

---
+++ app/src/main/java/com/example/segundo_intento/practica.kt
@@ -0,0 +1,41 @@
+package com.example.segundo_intento
+
+//fun main(){
+    //val name ="Lesly"
+  //  val color = "morado"
+    // comentario
+//println("$name su color fav es el $color" )
+//}
+
+//fun main (){
+//val naranjas = 20
+//val uvas = 10
+
+//val total = (5*naranjas) + (3*uvas)
+
+//println("el total es $total")
+//}
+
+// funcion = fun
+fun main () {
+    // Valores Entertos - Int
+    val age :Int = 33
+    // los espacios que dejan se llaman identacion
+    // Long = cadena de caracteres img.
+    val examplelong:Long =30
+    // Valores Flotantes
+    val exampleFloat:Float = 30.5f
+    //println(examplelong)
+    //Double
+    val doubleExample:Double = 3241.3123245
+    //print(examplelong)
+    //variabls alfanumericas
+    val charexample1:Char ='@'
+    val charexample:Char ='2'
+    //print("$charexample1 y $charexample")
+    val booleanEjemplo:Boolean = true
+    print(booleanEjemplo)
+
+
+}
+
